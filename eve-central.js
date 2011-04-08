c6af65f69c873557b5d6ac966b947c984443ede7
Aragaer <aragaer@gmail.com>
2011-04-08 22:10:27 +0400

    Synchronous getPrice2 works now

--- eve-central.js
+++ eve-central.js
@@ -5,10 +5,15 @@ Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 Components.utils.import("resource://gre/modules/Services.jsm");
 
 const Query = {
-    getPrice: "select price from simple_prices where typeID=:tid " +
+    getPriceSimple: "select price from simple_prices where typeID=:tid " +
             "and date('now') < date(exp_date);",
-    setPrice: "replace into simple_prices values(:tid, :price, " +
+    getPrice: "select data from eve_central_prices where typeID=:tid " +
+            "and hashstring=:hash and date('now') < date(exp_date);",
+    setPriceSimple: "replace into simple_prices values(:tid, :price, " +
             "date('now', '+7 days'));",
+    setPrice: "replace into eve_central_prices " +
+            "(typeID, hashstring, data, exp_date) " +
+            "values(:tid, :hash, :data, date('now', '+7 days'));",
 };
 const Stm = {};
 var gEC;
@@ -35,15 +40,13 @@ evecentral.prototype = {
     get name()          "EVE Central",
 
     getPriceForItemAsync:   function (typeID, params, handler) {
-        var price = getPriceFromDB(typeID);
+        var price = getPriceSimpleFromDB(typeID);
         if (price != -1) {
             handler.onData(price);
             return true;
         }
         var data = prepareData(typeID, params);
-        var req = makeReq();
-        req.open('POST', 'http://api.eve-central.com/api/marketstat', true);
-        req.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+        var req = makeReq(true);
         req.onreadystatechange = function (aEvt) {
             if (req.readyState == 4)
                 handler.onData(processResult(req, params, typeID));
@@ -52,16 +55,15 @@ evecentral.prototype = {
         return true;
     },
     getPriceForItemAsync2:  function (typeID, params, handler) {
+        var data = prepareData(typeID, params);
         return false;
     },
     getPriceForItem:    function (typeID, params) {
-        var price = getPriceFromDB(typeID);
-        if (price != -1) {
-            handler.onData(price);
-            return true;
-        }
+        var price = getPriceSimpleFromDB(typeID);
+        if (price != -1)
+            return price;
         var data = prepareData(typeID, params);
-        var req = makeReq();
+        var req = makeReq(false);
         req.open('POST', 'http://api.eve-central.com/api/marketstat', false);
         req.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
         try {
@@ -76,7 +78,22 @@ evecentral.prototype = {
         return processResult(req, params, typeID);
     },
     getPriceForItem2:   function (typeID, params) {
-        return { wrappedJSObject: null };
+        var data = prepareData(typeID, params);
+        var hash = makeHash(data);
+        var price_data = getPriceFromDB(typeID, hash);
+        if (price_data)
+            return {wrappedJSObject: JSON.parse(price_data)};
+        var req = makeReq(false);
+        try {
+            var t = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
+            t.initWithCallback(req.abort, 5000, t.TYPE_ONE_SHOT);
+            req.send(data);
+            t.cancel();
+        } catch (e) {
+            dump("Requesting data from eve central: "+e+"\n");
+            req = {status: 0};
+        }
+        return {wrappedJSObject: processResult2(req, typeID, hash)};
     },
     observe:        function (aSubject, aTopic, aData) {
         dump('Got '+aTopic+' event in eve central\n');
@@ -99,20 +116,28 @@ function init() {
     if (!gEC._conn.tableExists('simple_prices'))
         gEC._conn.createTable('simple_prices',
                 'typeID integer, price float, exp_date char, primary key (typeID)');
-    for (var i in Query)
-        try {
+    if (!gEC._conn.tableExists('eve_central_prices'))
+        gEC._conn.createTable('eve_central_prices',
+                'typeID integer, exp_date char, hashstring char, data char, ' +
+                'primary key (typeID, hashstring)');
+    try {
+        for (var i in Query)
             Stm[i] = gEC._conn.createStatement(Query[i]);
-        } catch (e) {
-            dump(gEC._conn.lastErrorString+"\n");
-        }
+    } catch (e) {
+        dump(gEC._conn.lastErrorString+"\n");
+    }
     dump("Eve central init done\n");
 }
 
-function makeReq()
-    Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance(Ci.nsIXMLHttpRequest);
+function makeReq(async) {
+    var req = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance(Ci.nsIXMLHttpRequest);
 
-function getPriceFromDB(typeID) {
-    let stm = Stm.getPrice;
+    req.open('POST', 'http://api.eve-central.com/api/marketstat', async);
+    req.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+    return req;
+}
+function getPriceSimpleFromDB(typeID) {
+    let stm = Stm.getPriceSimple;
     stm.params.tid = typeID;
     try {
         if (stm.step())
@@ -125,8 +150,23 @@ function getPriceFromDB(typeID) {
     return -1;
 }
 
+function getPriceFromDB(typeID, hash) {
+    let stm = Stm.getPrice;
+    stm.params.tid = typeID;
+    stm.params.hash = hash;
+    try {
+        if (stm.step())
+            return stm.row.data;
+    } catch (e) {
+        dump(""+e+"\n");
+    } finally {
+        stm.reset();
+    }
+    return null;
+}
+
 function writePriceToDB(typeID, price) {
-    let stm = Stm.setPrice;
+    let stm = Stm.setPriceSimple;
     try {
         stm.params.tid = typeID;
         stm.params.price = price;
@@ -137,6 +177,49 @@ function writePriceToDB(typeID, price) {
         stm.reset();
     }
 }
+
+
+function writePriceToDB2(typeID, data, hash) {
+    let stm = Stm.setPrice;
+    try {
+        stm.params.tid = typeID;
+        stm.params.data = data;
+        stm.params.hash = hash;
+        stm.execute();
+    } catch (e) {
+        dump(""+e+"\n");
+    } finally {
+        stm.reset();
+    }
+}
+
+function processResult2(req, typeID, hash) {
+    var res, xpe_res;
+    var result = {buy: {}, sell: {}, all: {}};
+    dump(req+"\n");
+    if (req.status != 200) {
+        dump('Failed to connect to server!\n');
+        Services.obs.notifyObservers(null, 'eve-market-error', 'Failed to connect to server '+req.status);
+        return -1;
+    }
+
+    var xpe = Cc["@mozilla.org/dom/xpath-evaluator;1"].
+            createInstance(Ci.nsIDOMXPathEvaluator);
+    var nsResolver = xpe.createNSResolver(req.responseXML.documentElement);
+    try {
+        xpe_res = xpe.evaluate('//min | //max | //median | //average', req.responseXML, nsResolver, 0, null);
+    } catch (e) {
+        dump("error running xpe: "+e+"\n");
+        return null;
+    }
+
+    while (res = xpe_res.iterateNext())
+        result[res.parentNode.tagName][res.tagName] = +res.textContent;
+
+    writePriceToDB2(typeID, JSON.stringify(result), hash)
+    return result;
+}
+
 function processResult(req, params, typeID) {
     var field = params.req || "//all/median";
     if (req.status != 200) {
@@ -162,6 +245,7 @@ function processResult(req, params, typeID) {
         return res.data;
     } else
         return -1;
+    return result;
 }
 
 const stringparams = 'hours minQ usesystem'.split(' ');
@@ -181,3 +265,18 @@ function prepareData(typeID, params) {
     return data.join('&');
 }
 
+function makeHash(str) {
+    var converter = Cc["@mozilla.org/intl/scriptableunicodeconverter"].
+            createInstance(Ci.nsIScriptableUnicodeConverter);
+    converter.charset = "UTF-8";
+    var result = {};
+    // data is an array of bytes
+    var data = converter.convertToByteArray(str, result);
+    var hasher = Cc["@mozilla.org/security/hash;1"].createInstance(Ci.nsICryptoHash);
+    hasher.init(hasher.MD5);
+    hasher.update(data, data.length);
+    var hash = hasher.finish(false);
+    return [toHexString(hash.charCodeAt(i)) for (i in hash)].join("");
+}
+
+function toHexString(c) ("00" + c.toString(16)).slice(-2)
